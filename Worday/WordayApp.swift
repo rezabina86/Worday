86e231892631219f405a2b6e835907bc2e2ca9e1
Reza Bina <tavakolibina@gmail.com>
2024-12-23 13:35:53 +0100

    initial commit

---
+++ Worday/WordayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordayApp.swift
+//  Worday
+//
+//  Created by Reza Bina on 23.12.24.
+//
+
+import SwiftUI
+
+@main
+struct WordayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordayTests/WordayTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordayTests.swift
+//  WordayTests
+//
+//  Created by Reza Bina on 23.12.24.
+//
+
+import Testing
+@testable import Worday
+
+struct WordayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
