ab308fad81335fed7bfb37cf7deb19ad9740a9dd
Reza Bina <tavakolibina@gmail.com>
2024-12-30 14:10:53 +0100

    [9] Word Provider (#9)
    Major changes to WordProvider

--- WordayTests/Mocks/WordProviderUseCaseMock.swift
+++ WordayTests/Mocks/WordProviderUseCaseMock.swift
@@ -1,14 +1,19 @@
 @testable import Worday
 import Foundation
 
-final class FetchWordUseCaseMock: FetchWordUseCaseType {
+final class WordProviderUseCaseMock: WordProviderUseCaseType {
     enum Call: Equatable {
         case fetch
+        case store(word: String)
     }
     
     func fetch() -> FetchWordModel {
         calls.append(.fetch)
         return fetchReturnValue
+    }
+    
+    func store(word: String) {
+        calls.append(.store(word: word))
     }
     
     var calls: [Call] = []
