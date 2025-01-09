97afdf203aea305ab04c17a7d60b573fefeb075b
Reza Bina <tavakolibina@gmail.com>
2025-01-09 17:21:26 +0100

    Add test for GameViewModel

---
+++ WordayTests/Tests/View Models/GameViewModelTests.swift
@@ -0,0 +1,92 @@
+import Combine
+import Testing
+import Foundation
+@testable import Worday
+
+final class GameViewModelTests {
+    var sut: GameViewModel!
+    var mockWordProviderUseCase: WordProviderUseCaseMock
+    var mockOngoingGameViewModelFactory: OngoingGameViewModelFactoryMock
+    var mockFinishedGameViewModelFactory: FinishedGameViewModelFactoryMock
+    var mockScenePhaseObserver: ScenePhaseObserverMock
+    var mockAppTriggerFactory: AppTriggerFactoryMock
+    var mockModalCoordinator: ModalCoordinatorMock
+    
+    var cancellables: Set<AnyCancellable>
+    var viewState: GameViewState?
+    
+    init() {
+        cancellables = []
+        mockWordProviderUseCase = .init()
+        mockOngoingGameViewModelFactory = .init()
+        mockFinishedGameViewModelFactory = .init()
+        mockScenePhaseObserver = .init()
+        mockAppTriggerFactory = .init()
+        mockModalCoordinator = .init()
+        
+        sut = .init(
+            wordProviderUseCase: mockWordProviderUseCase,
+            ongoingGameViewModelFactory: mockOngoingGameViewModelFactory,
+            finishedGameViewModelFactory: mockFinishedGameViewModelFactory,
+            scenePhaseObserver: mockScenePhaseObserver,
+            appTriggerFactory: mockAppTriggerFactory,
+            modalCoordinator: mockModalCoordinator
+        )
+        
+        sut.viewState
+            .sink { [weak self] state in
+                self?.viewState = state
+            }
+            .store(in: &cancellables)
+    }
+    
+    @Test func testCreateWithWord() async throws {
+        mockWordProviderUseCase.fetchReturnValue = .word(word: "abcde")
+        mockAppTriggerFactory.triggerRelay.send(())
+        #expect(mockOngoingGameViewModelFactory.calls == [.create(word: "abcde")])
+        #expect(mockFinishedGameViewModelFactory.calls.isEmpty)
+        #expect(viewState?.isGame ?? false)
+    }
+    
+    @Test func testCreateWithoutWord() async throws {
+        mockWordProviderUseCase.fetchReturnValue = .noWordToday(lastPlayedWord: "abcde")
+        mockAppTriggerFactory.triggerRelay.send(())
+        #expect(mockOngoingGameViewModelFactory.calls.isEmpty)
+        #expect(mockFinishedGameViewModelFactory.calls == [.create(word: "abcde")])
+        #expect(viewState?.isNotGameToday ?? false)
+    }
+    
+    @Test func testCreateWithError() async throws {
+        mockWordProviderUseCase.fetchReturnValue = .error
+        mockAppTriggerFactory.triggerRelay.send(())
+        #expect(mockOngoingGameViewModelFactory.calls.isEmpty)
+        #expect(mockFinishedGameViewModelFactory.calls.isEmpty)
+        #expect(viewState == .error)
+    }
+    
+    @Test func testPresentInfoModal() async throws {
+        sut.setModalDestination(.info)
+        #expect(mockModalCoordinator.calls == [.present(destination: .info)])
+    }
+    
+    @Test func testPhaseChanged() async throws {
+        sut.scenePhaseChanged(.active)
+        #expect(mockScenePhaseObserver.calls == [.phaseChanged(scenePhase: .active)])
+    }
+}
+
+private extension GameViewState {
+    var isGame: Bool {
+        switch self {
+        case .game: return true
+        case .empty, .error, .noWordToday: return false
+        }
+    }
+    
+    var isNotGameToday: Bool {
+        switch self {
+        case .empty, .error, .game: return false
+        case .noWordToday: return true
+        }
+    }
+}
